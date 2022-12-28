8e1f80d1cac8696a2d6a7ff3bb7c9fcc2d908424
Joo <vijak13019@hotmail.com>
2022-12-28 17:03:01 +0700

    fix(App): Change Title and url of it to nothing

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,9 @@ export default function App() {
     <main className="flex min-h-screen flex-col bg-bright-navy-blue font-neucha">
       <a
         className="mt-8 list-none self-center text-5xl text-ghost-white"
-        href="https://google.com"
+        href="#"
       >
-        Taskify
+        Taskify V2
       </a>
       <InputBox />
       <Task />
