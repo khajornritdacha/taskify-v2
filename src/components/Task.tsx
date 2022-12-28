a11227725708db61049b28b133918c1aa4b0d903
Joo <vijak13019@hotmail.com>
2022-12-28 17:03:35 +0700

    feat: add Completed tasks

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -4,8 +4,8 @@ const Task = () => {
   let tasks = ['hello', 'how', 'are', 'you'];
   return (
     <div className="grid grid-cols-2 justify-items-center px-[3%]">
-      <TaskContainer tasks={tasks.slice(0, 3)} />
-      <TaskContainer tasks={[tasks[3]]} />
+      <TaskContainer tasks={tasks.slice(0, 3)} isCompleted={false} />
+      <TaskContainer tasks={[tasks[3]]} isCompleted={true} />
     </div>
   );
 };
--- src/components/TaskContainer.tsx
+++ src/components/TaskContainer.tsx
@@ -2,16 +2,21 @@ import SingleTask from './SingleTask';
 
 interface Props {
   tasks: string[];
+  isCompleted: boolean;
 }
 
-const TaskContainer: React.FC<Props> = ({ tasks }) => {
+const TaskContainer: React.FC<Props> = ({ tasks, isCompleted }) => {
   return (
-    <div className="h-fit min-w-[85%] rounded-lg bg-maximum-blue-green">
+    <div
+      className={`h-fit min-w-[85%] rounded-lg ${
+        !isCompleted ? 'bg-maximum-blue-green' : 'bg-orange-red-crayola'
+      }`}
+    >
       <h1 className="mt-4 py-2 text-center text-4xl font-bold text-ghost-white">
-        Active Tasks
+        {!isCompleted ? 'Active Tasks' : 'Completed Tasks'}
       </h1>
       {tasks.map((task) => (
-        <SingleTask task={task} />
+        <SingleTask task={task} isCompleted={isCompleted} />
       ))}
     </div>
   );
