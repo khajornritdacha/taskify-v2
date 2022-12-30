f5b7c68469282a5cfacf16480daf6503376e944b
Joo <vijak13019@hotmail.com>
2022-12-30 09:34:19 +0700

    feat(mainPage): add refresh token and logout button for dev

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -1,13 +1,28 @@
 import InputBox from './InputBox';
 import Task from './Task';
-
-import { Link } from 'react-router-dom';
+import { useAuth } from '../providers/AuthProvider';
 
 const MainPage = () => {
+  const { logout } = useAuth();
+
+  const refreshToken = async () => {};
+
   return (
     <>
       <InputBox />
       <Task />
+      <button
+        className="mx-auto mt-2 max-w-fit rounded-full bg-ghost-white p-3"
+        onClick={refreshToken}
+      >
+        Refresh my Token
+      </button>
+      <button
+        className="mx-auto mt-2 max-w-fit rounded-full bg-ghost-white p-3"
+        onClick={logout}
+      >
+        logout
+      </button>
     </>
   );
 };
