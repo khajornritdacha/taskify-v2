2f87a41690caad3ed0ad1fca0da7e9710ba663da
Joo <vijak13019@hotmail.com>
2023-01-02 21:07:10 +0700

    feat(DataProvider): add handle error

--- src/providers/DataProvider.tsx
+++ src/providers/DataProvider.tsx
@@ -3,6 +3,8 @@ import { Todo } from '../models/model';
 import { IGetResponse } from '../models/apiModel';
 import { useAuth } from './AuthProvider';
 import { api } from '../utils/axios';
+import axios, { AxiosError } from 'axios';
+import { ErrorDto } from '../models/model';
 
 export const DataContext = createContext<IDataContext | null>(null);
 
@@ -32,20 +34,34 @@ const DataProvider = (props: DataProviderProps) => {
       setTodos(res.data?.todos);
       setCompletedTodos(res.data?.toRemoves);
     } catch (err) {
-      console.log('Refresh data err');
-      console.log(err);
       setTodos([]);
       setCompletedTodos([]);
+
+      if (axios.isAxiosError(err)) {
+        const { response } = err as AxiosError<ErrorDto>;
+        const message = response?.data.message;
+        if (message) throw new Error(message);
+      }
+      throw new Error('Unknown Error');
     }
   };
 
   const addData = async (todo: string) => {
-    const res = await api.post('/api/todos', {
-      todoText: todo,
-      isDone: false,
-    });
-    console.log('Add data: ', res);
-    await refreshData();
+    try {
+      const res = await api.post('/api/todos', {
+        todoText: todo,
+        isDone: false,
+      });
+      console.log('Add data: ', res);
+      await refreshData();
+    } catch (err) {
+      if (axios.isAxiosError(err)) {
+        const { response } = err as AxiosError<ErrorDto>;
+        const message = response?.data.message;
+        if (message) throw new Error(message);
+      }
+      throw new Error('Unknown Error');
+    }
   };
 
   return (
