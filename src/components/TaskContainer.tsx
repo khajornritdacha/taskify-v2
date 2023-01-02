9b46f1b7f223d57b5c555a5142ba19539a1ed2dc
Kamanun Maneesri <jomlovejen7956@gmail.com>
2023-01-02 15:35:31 +0700

    added snapshot (but nothing changed)

--- src/components/TaskContainer.tsx
+++ src/components/TaskContainer.tsx
@@ -16,11 +16,11 @@ const TaskContainer: React.FC<Props> = ({ tasks, isCompleted, todos, setTodos, c
 
   return <div className="container">
     {enabled && <Droppable droppableId={isCompleted ? `CompletedTodosList` : `UnCompletedTodosList`}>
-      {(provided) => (
+      {(provided, snapshot) => (
         <div
           className={`h-fit min-w-[95%] rounded-lg ${
             !isCompleted ? 'bg-maximum-blue-green' : 'bg-orange-red-crayola'
-          }`} ref={provided.innerRef} {...provided.droppableProps}
+          } ${snapshot.isDraggingOver ? 'dragactive' : ''}`} ref={provided.innerRef} {...provided.droppableProps}
         >
           <h1 className="mt-4 py-2 text-center text-4xl font-bold text-ghost-white">
             {!isCompleted ? 'Active Tasks' : 'Completed Tasks'}
